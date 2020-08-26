7b7d8fd9a6174b999c3e14a0e0955dd30c3ef615
Hans <741581879@qq.com>
2020-08-26 19:28:52 +0800

    [mocking practice][Hans Ray] test: Implete testPark_givenAVipCarAndAFullParkingLog_thenGiveAReceiptWithCarNameAndParkingLotName test

--- src/test/java/parking/VipParkingStrategyTest.java
+++ src/test/java/parking/VipParkingStrategyTest.java
@@ -1,24 +1,44 @@
 package parking;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
 
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
+@RunWith(MockitoJUnitRunner.class)
 
 public class VipParkingStrategyTest {
+    private static final String PARKING_LOT_NAME_A = "A";
+    private static final String PARKING_LOT_NAME_B = "B";
+
+    public static String NO_PARKING_LOT = "No Parking Lot";
+    private static final String CAR_NAME_OL1234 = "OL1234";
 
 	@Test
     public void testPark_givenAVipCarAndAFullParkingLog_thenGiveAReceiptWithCarNameAndParkingLotName() {
-
 	    /* Exercise 4, Write a test case on VipParkingStrategy.park()
 	    * With using Mockito spy, verify and doReturn */
+        Car car = spy(new Car(CAR_NAME_OL1234));
+        ParkingLot parkingLot = spy(new ParkingLot(PARKING_LOT_NAME_A, 1));
+        List<ParkingLot> parkingLots = Collections.singletonList(parkingLot);
+        VipParkingStrategy vipParkingStrategy = spy(new VipParkingStrategy());
+        doReturn(true).when(vipParkingStrategy).isAllowOverPark(any());
 
+        Receipt receipt = vipParkingStrategy.park(parkingLots, car);
+        assertEquals(PARKING_LOT_NAME_A, receipt.getParkingLotName());
+        verify(parkingLot, times(1)).getName();
+        verify(car, times(1)).getName();
     }
 
     @Test
     public void testPark_givenCarIsNotVipAndAFullParkingLog_thenGiveNoSpaceReceipt() {
-
         /* Exercise 4, Write a test case on VipParkingStrategy.park()
          * With using Mockito spy, verify and doReturn */
+
     }
 
     @Test
